400900cb77519877f952bc52c564f6740bf81d33
agadro <adridrozdziel@gmail.com>
2020-03-08 15:38:01 +0100

    init

---
+++ Areas/Identity/Data/BlogUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Blog.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the BlogUser class
+    public class BlogUser : IdentityUser
+    {
+    }
+}
